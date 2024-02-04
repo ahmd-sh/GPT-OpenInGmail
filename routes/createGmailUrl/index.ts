4362f6e478bc5cb6f3dda3e3461046e229686547
Ahmed Shaikh <42516500+ahmd-sh@users.noreply.github.com>
2024-02-04 15:59:36 -0700

    Update index.ts

--- routes/createGmailUrl/index.ts
+++ routes/createGmailUrl/index.ts
@@ -1,12 +1,13 @@
 export default eventHandler((event) => {
   const subjectLine = getQuery(event).subjectLine || "undefinedSubject";
   const emailBody = getQuery(event).emailBody || "undefinedBody";
+  const accountId = getQuery(event).emailBody || 0;
 
   // Ensure inputs are URL-safe
   const encodedSubject = encodeURIComponent(subjectLine.toString());
   const encodedBody = encodeURIComponent(emailBody.toString());
 
-  const oigUrl = `https://mail.google.com/mail/u/0/?tf=cm&fs=0&su=${encodedSubject}&body=${encodedBody}`;
+  const oigUrl = `https://mail.google.com/mail/u/${accountId}/?tf=cm&fs=0&su=${encodedSubject}&body=${encodedBody}`;
 
   return {
     operationId: "createGmailComposeUrl",
